fbaf930ee8ac59db0b74e7d7e2e76b439df5d28f
tomkeyzhang <tomkeyzhang@gmail.com>
2013-09-18 09:47:38 +0800

    demo修改

--- Host/src/com/dianping/loader/MainActivity.java
+++ Host/src/com/dianping/loader/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.dianping.loader;
 
 import android.app.Fragment;
@@ -31,173 +32,172 @@ import com.dianping.loader.model.SiteSpec;
  * _fragment:String，Fragment的类名<br>
  * 
  * @author Yimin
- * 
  */
 public class MainActivity extends MyActivity {
-	private SiteSpec site;
-	private FileSpec file;
-	private String fragmentName;
-	private boolean loaded;
-	private MyClassLoader classLoader;
-	private MyResources myResources;
-	private AssetManager assetManager;
-	private Resources resources;
-	private Theme theme;
-	private FrameLayout rootView;
+    private SiteSpec site;
+    private FileSpec file;
+    private String fragmentName;
+    private boolean loaded;
+    private MyClassLoader classLoader;
+    private MyResources myResources;
+    private AssetManager assetManager;
+    private Resources resources;
+    private Theme theme;
+    private FrameLayout rootView;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		Intent intent = getIntent();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
 
-		int error = 0;
-		// must be load at the first start
-		do {
-			site = intent.getParcelableExtra("_site");
-			if (site == null) {
-				error = 201; // #201
-				break;
-			}
-			fragmentName = intent.getStringExtra("_fragment");
-			if (TextUtils.isEmpty(fragmentName)) {
-				error = 202; // #202
-				break;
-			}
-			String code = intent.getStringExtra("_code");
-			if (TextUtils.isEmpty(code)) {
-				loaded = true;
-				break;
-			}
-			file = site.getFile(code);
-			if (file == null) {
-				error = 205; // #205
-				break;
-			}
-			classLoader = MyClassLoader.getClassLoader(site, file);
-			loaded = classLoader != null;
-			if (!loaded) {
-				error = 210; // #210
-				break;
-			}
-		} while (false);
+        int error = 0;
+        // must be load at the first start
+        do {
+            site = intent.getParcelableExtra("_site");
+            if (site == null) {
+                error = 201; // #201
+                break;
+            }
+            fragmentName = intent.getStringExtra("_fragment");
+            if (TextUtils.isEmpty(fragmentName)) {
+                error = 202; // #202
+                break;
+            }
+            String code = intent.getStringExtra("_code");
+            if (TextUtils.isEmpty(code)) {
+                loaded = true;
+                break;
+            }
+            file = site.getFile(code);
+            if (file == null) {
+                error = 205; // #205
+                break;
+            }
+            classLoader = MyClassLoader.getClassLoader(site, file);
+            loaded = classLoader != null;
+            if (!loaded) {
+                error = 210; // #210
+                break;
+            }
+        } while (false);
 
-		super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
 
-		rootView = new FrameLayout(this);
-		rootView.setLayoutParams(new ViewGroup.LayoutParams(
-				ViewGroup.LayoutParams.MATCH_PARENT,
-				ViewGroup.LayoutParams.MATCH_PARENT));
-		rootView.setId(android.R.id.primary);
-		setContentView(rootView);
+        rootView = new FrameLayout(this);
+        rootView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        rootView.setId(android.R.id.primary);
+        setContentView(R.layout.fragment);
 
-		if (!loaded) {
-			TextView text = new TextView(this);
-			text.setText("无法载入页面" + (error == 0 ? "" : " #" + error));
-			text.setLayoutParams(new FrameLayout.LayoutParams(
-					ViewGroup.LayoutParams.WRAP_CONTENT,
-					ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
-			rootView.addView(text);
+        if (!loaded) {
+            TextView text = new TextView(this);
+            text.setText("无法载入页面" + (error == 0 ? "" : " #" + error));
+            text.setLayoutParams(new FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+            rootView.addView(text);
 
-			return;
-		}
+            return;
+        }
 
-		// the fragment will be restored by framework
-		if (savedInstanceState != null)
-			return;
+        // the fragment will be restored by framework
+        if (savedInstanceState != null)
+            return;
 
-		Fragment fragment = null;
-		try {
-			fragment = (Fragment) getClassLoader().loadClass(fragmentName)
-					.newInstance();
-		} catch (Exception e) {
-			loaded = false;
-			classLoader = null;
-			error = 211; // #211
-			TextView text = new TextView(this);
-			text.setText("无法载入页面 #" + error);
-			text.append("\n" + e);
-			text.setLayoutParams(new FrameLayout.LayoutParams(
-					ViewGroup.LayoutParams.WRAP_CONTENT,
-					ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
-			rootView.addView(text);
+        Fragment fragment = null;
+        try {
+            fragment = (Fragment) getClassLoader().loadClass(fragmentName)
+                    .newInstance();
+        } catch (Exception e) {
+            loaded = false;
+            classLoader = null;
+            error = 211; // #211
+            TextView text = new TextView(this);
+            text.setText("无法载入页面 #" + error);
+            text.append("\n" + e);
+            text.setLayoutParams(new FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+            rootView.addView(text);
 
-			return;
-		}
+            return;
+        }
 
-		FragmentManager fm = getFragmentManager();
-		FragmentTransaction ft = fm.beginTransaction();
-		ft.replace(android.R.id.primary, fragment);
-		ft.commit();
-	}
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(android.R.id.primary, fragment);
+        ft.commit();
+    }
 
-	@Override
-	public ClassLoader getClassLoader() {
-		return classLoader == null ? super.getClassLoader() : classLoader;
-	}
+    @Override
+    public ClassLoader getClassLoader() {
+        return classLoader == null ? super.getClassLoader() : classLoader;
+    }
 
-	public SiteSpec getSite() {
-		return site;
-	}
+    public SiteSpec getSite() {
+        return site;
+    }
 
-	public FileSpec getFile() {
-		return file;
-	}
+    public FileSpec getFile() {
+        return file;
+    }
 
-	public String getFragmentName() {
-		return fragmentName;
-	}
+    public String getFragmentName() {
+        return fragmentName;
+    }
 
-	@Override
-	public Intent urlMap(Intent intent) {
-		do {
-			// only process my scheme uri
-			Uri uri = intent.getData();
-			if (uri == null)
-				break;
-			if (uri.getScheme() == null)
-				break;
-			if (!(MyApplication.PRIMARY_SCHEME
-					.equalsIgnoreCase(uri.getScheme())))
-				break;
+    @Override
+    public Intent urlMap(Intent intent) {
+        do {
+            // only process my scheme uri
+            Uri uri = intent.getData();
+            if (uri == null)
+                break;
+            if (uri.getScheme() == null)
+                break;
+            if (!(MyApplication.PRIMARY_SCHEME
+                    .equalsIgnoreCase(uri.getScheme())))
+                break;
 
-			if (!intent.hasExtra("_site")) {
-				intent.putExtra("_site", site);
-			}
-		} while (false);
-		return super.urlMap(intent);
-	}
+            if (!intent.hasExtra("_site")) {
+                intent.putExtra("_site", site);
+            }
+        } while (false);
+        return super.urlMap(intent);
+    }
 
-	@Override
-	public AssetManager getAssets() {
-		return assetManager == null ? super.getAssets() : assetManager;
-	}
+    @Override
+    public AssetManager getAssets() {
+        return assetManager == null ? super.getAssets() : assetManager;
+    }
 
-	@Override
-	public Resources getResources() {
-		return resources == null ? super.getResources() : resources;
-	}
+    @Override
+    public Resources getResources() {
+        return resources == null ? super.getResources() : resources;
+    }
 
-	@Override
-	public Theme getTheme() {
-		return theme == null ? super.getTheme() : theme;
-	}
+    @Override
+    public Theme getTheme() {
+        return theme == null ? super.getTheme() : theme;
+    }
 
-	public MyResources getOverrideResources() {
-		return myResources;
-	}
+    public MyResources getOverrideResources() {
+        return myResources;
+    }
 
-	void setOverrideResources(MyResources myres) {
-		if (myres == null) {
-			this.myResources = null;
-			this.resources = null;
-			this.assetManager = null;
-			this.theme = null;
-		} else {
-			this.myResources = myres;
-			this.resources = myres.getResources();
-			this.assetManager = myres.getAssets();
-			Theme t = myres.getResources().newTheme();
-			t.setTo(getTheme());
-			this.theme = t;
-		}
-	}
+    void setOverrideResources(MyResources myres) {
+        if (myres == null) {
+            this.myResources = null;
+            this.resources = null;
+            this.assetManager = null;
+            this.theme = null;
+        } else {
+            this.myResources = myres;
+            this.resources = myres.getResources();
+            this.assetManager = myres.getAssets();
+            Theme t = myres.getResources().newTheme();
+            t.setTo(getTheme());
+            this.theme = t;
+        }
+    }
 }
--- legacy/FragmentDeveloper/src/com/dianping/example/fragment/SampleFragment.java
+++ legacy/FragmentDeveloper/src/com/dianping/example/fragment/SampleFragment.java
@@ -1,7 +1,9 @@
+
 package com.dianping.example.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,36 +12,36 @@ import android.widget.Button;
 import com.dianping.example.fragmentdeveloper.R;
 
 public class SampleFragment extends Fragment {
-	Button btn;
-	int counter;
+    Button btn;
+    int counter;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.sample_fragment, container, false);
-		btn = (Button) v.findViewById(R.id.btn);
-		btn.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				btn.setText(String.valueOf(++counter));
-			}
-		});
-		if (savedInstanceState != null) {
-			counter = savedInstanceState.getInt("counter");
-		}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.sample_fragment, container, false);
+        btn = (Button) v.findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                btn.setText(String.valueOf(++counter));
+            }
+        });
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt("counter");
+        }
+        Log.d("zqt", getActivity() + "");
+        btn.setText(String.valueOf(counter));
+        return v;
+    }
 
-		btn.setText(String.valueOf(counter));
-		return v;
-	}
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
+    }
 
-	}
-
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		outState.putInt("counter", counter);
-	}
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter", counter);
+    }
 }
