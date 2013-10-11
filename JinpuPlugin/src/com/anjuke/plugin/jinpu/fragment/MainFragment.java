92c3404a848d695ed9ad4872875befa31fc4d67d
tomkeyzhang <tomkeyzhang@gmail.com>
2013-10-11 13:53:55 +0800

    修改PluginActivity

--- JinpuPlugin/src/com/anjuke/plugin/jinpu/fragment/MainFragment.java
+++ JinpuPlugin/src/com/anjuke/plugin/jinpu/fragment/MainFragment.java
@@ -1,17 +1,29 @@
 
 package com.anjuke.plugin.jinpu.fragment;
 
-import com.anjuke.jinpu.plugin.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.anjuke.jinpu.plugin.R;
+import com.anjuke.plugin.jinpu.PluginActivity;
+
 public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.start_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(PluginActivity.getLaunchIntent(getActivity(), HomeFragment.class.getName()));
+            }
+        });
     }
 }
